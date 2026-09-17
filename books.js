a1f9fd0718adb780d9e545c689d78db151e94508
Fathia <main.nurfathia@gmail.com>
2026-09-17 20:05:27 +0700

    Update Link URL format in books.js

--- books.js
+++ books.js
@@ -127,7 +127,7 @@ const books = [
     spoiler: true,
     category: "fiction",
     // Link: "https://x.com/readbymoon/status/2043946525571858772?s=20",
-    Link: "https://www.instagram.com/p/DdHEzP0IDG7/?utm_source=ig_web_copy_link&stkn=MzRlODBiNWFlZA==",
+    Link: "https://www.instagram.com/p/DdHEzP0IDG7/",
     image: "https://pbs.twimg.com/media/HF2ABs2bYAADWud?format=jpg&name=medium"
   },
 ];
